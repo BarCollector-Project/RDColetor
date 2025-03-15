778b32659557e412c301b90d01d4d519645f94a4
XayUp <lucasjrcostadacosta@gmail.com>
2025-03-15 19:57:34 -0400

    Nova Atividade

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
   runApp(const Main());
@@ -26,31 +27,61 @@ class ScannerPage extends StatefulWidget {
 }
 
 class QRScannerPageState extends State<ScannerPage> {
-  late final WebViewController _webviewController;
+  late WebViewController _webViewController;
   MobileScannerController _scannerController = MobileScannerController();
 
-  bool _isInitialized = false;
-  String? _qrCode = "Nenhum QR Code detectado";
-
   bool _exibirScanner = false;
-  String _codigoBarras = '';
-
-  Future<void> _scanBarcode() async {
-    String barcodeScanRes;
-  }
-
-  // Função para iniciar a câmera
-  void _startCamera() {}
-
-  // Função para alternar o foco manual
-  void _focus() async {}
 
   @override
   void initState() {
-    _webviewController =
+    _webViewController =
         WebViewController()
           ..setJavaScriptMode(JavaScriptMode.unrestricted)
-          ..loadRequest(Uri.parse('http://192.168.10.41:8080'));
+          ..setNavigationDelegate(
+            NavigationDelegate(
+              onPageFinished: (q) {
+                debugPrint("A página $q foi carregada!");
+                _webViewController.runJavaScript('''
+                    function simulateKeyPress(text) {
+                    var targetElement = document.body;//.getElementById('guac-display') || document.querySelector('canvas');
+
+                    //if (targetElement) {
+
+                      var eventKeyDown = new KeyboardEvent('keydown', {
+                        bubbles: true,
+                        cancelable: true,
+                        key: text, // Usamos 'key' para a string
+                        keyCode: text.charCodeAt(0),
+                        char: text
+                      });
+                      targetElement.dispatchEvent(eventKeyDown);
+
+                      var eventKeyPress = new KeyboardEvent('keypress', {
+                        bubbles: true,
+                        cancelable: true,
+                        key: text, // Usamos 'key' para a string
+                        keyCode: text.charCodeAt(0),
+                        char: text
+                      });
+                      targetElement.dispatchEvent(eventKeyPress);
+
+                      var eventKeyUp = new KeyboardEvent('keyup', {
+                        bubbles: true,
+                        cancelable: true,
+                        key: text, // Usamos 'key' para a string
+                        keyCode: text.charCodeAt(0),
+                        char: text
+                      });
+                      targetElement.dispatchEvent(eventKeyUp);
+                    //} else {
+                      console.error('Elemento alvo não encontrado.');
+                    //}
+                  }
+                ''');
+              },
+            ),
+          )
+          ..loadRequest(Uri.parse("http://192.168.10.41:8080"));
     super.initState();
   }
 
@@ -60,32 +91,36 @@ class QRScannerPageState extends State<ScannerPage> {
     });
   }
 
-  Future<void> injectKeyboardEvent(String text) async {
-    List<int> unitText = text.codeUnits;
-    for (int i = 0; i < unitText.length; i++) {
-      int keyCode = unitText[i];
-      String jsCodeKeydown = """
-        var eventKeyDown = new KeyboardEvent('keydown', {
-          key: String.fromCharCode($keyCode),
-          keyCode: $keyCode,
-          which: $keyCode,
-          bubbles: true,
-          cancelable: true
-        });
-        document.dispatchEvent(eventKeyDown);
-      """;
-      String jsCodeKeyup = """
-        var eventKeyUp = new KeyboardEvent('keyup', {
-          key: String.fromCharCode($keyCode),
-          keyCode: $keyCode,
-          which: $keyCode,
-          bubbles: true,
-          cancelable: true
-        });
-        document.dispatchEvent(eventKeyUp);
-      """;
-      await _webviewController.runJavaScript(jsCodeKeydown);
-      await _webviewController.runJavaScript(jsCodeKeyup);
+  void sendCodeToWebView(String code) {
+    //_webViewController.runJavaScript(script);
+  }
+
+  Future<void> injectKeyboardEvent(String input) async {
+    // Injetar JavaScript para simular a entrada de teclado
+    String jsCode = '''
+
+      var event = new KeyboardEvent('keydown', {
+        key: '$input',
+        code: 'Key${input.toUpperCase()}',
+        char: '$input',
+        keyCode: '$input'.charCodeAt(0),
+        bubbles: true
+      });
+      document.dispatchEvent(event);
+    ''';
+    _webViewController?.runJavaScript(jsCode);
+  }
+
+  Future<void> _sendSequenceToWebView(String sequence) async {
+    // Itera sobre cada caractere na sequência
+    for (int i = 0; i < sequence.length; i++) {
+      String char = sequence[i];
+      // Chama a função JavaScript, passando o caractere
+      _webViewController.runJavaScript('simulateKeyPress("$char");');
+      // Adiciona um atraso
+      await Future.delayed(
+        const Duration(milliseconds: 10),
+      ); // Ajuste conforme necessário
     }
   }
 
@@ -95,40 +130,43 @@ class QRScannerPageState extends State<ScannerPage> {
     final telaAltura = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(title: Text("a, troço")),
-      body: Column(
-        children: <Widget>[
-          Expanded(child: WebViewWidget(controller: _webviewController)),
-          Container(
-            color: Colors.red,
-            width: telaLargura,
-            height: telaAltura * 0.15,
-            child:
-                (_exibirScanner)
-                    ? MobileScanner(
-                      controller: _scannerController,
-                      onDetect: (capture) async {
-                        final List<Barcode> barcodes = capture.barcodes;
-                        for (final barcode in barcodes) {
-                          String? barcodeString = barcode.rawValue;
-                          if (barcodeString != null) {
-                            debugPrint('Barcode found! ${barcode.rawValue}');
-                            //showScanner(false);
-                            _scannerController.stop();
-                            //await injectKeyboardEvent(barcodeString);
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(child: WebViewWidget(controller: _webViewController)),
+            Container(
+              color: Colors.red,
+              width: telaLargura,
+              height: telaAltura * 0.15,
+              child:
+                  (_exibirScanner)
+                      ? MobileScanner(
+                        controller: _scannerController,
+                        onDetect: (capture) async {
+                          final List<Barcode> barcodes = capture.barcodes;
+                          String? barcodeString;
+                          for (final barcode in barcodes) {
+                            barcodeString = barcode.rawValue;
+                            if (barcodeString != null) {
+                              debugPrint('Barcode found! ${barcode.rawValue}');
+                              showScanner(false);
+                              _scannerController.stop();
+                              sendCodeToWebView(barcodeString);
+                              break;
+                            }
                           }
-                        }
-                      },
-                    )
-                    : Expanded(
-                      child: ElevatedButton(
+                        },
+                      )
+                      : ElevatedButton(
                         onPressed: () {
-                          showScanner(true);
+                          _sendSequenceToWebView("TESTEE 1234567890");
+                          //showScanner(true);
                         },
                         child: Text('Iniciar Scanner'),
                       ),
-                    ),
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
